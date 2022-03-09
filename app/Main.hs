9014b4ba1313b527d919e871b2c44735a1b98402
omefire <omefire@omefires-MacBook-Pro.local>
2022-03-09 15:55:03 +0100

    Impmentation & testing of the LoadValue instruction

--- app/Main.hs
+++ app/Main.hs
@@ -14,42 +14,39 @@ import Control.Monad.Freer.TH
 import Control.Monad.Freer.Error
 import Control.Monad.Freer.State
 import Control.Monad.Freer.Writer
+import Data.Stack
 
 
 
 data ByteCode r where
     LoadValue :: Int -> ByteCode ()
-    WriteVariable :: String -> ByteCode ()
-    ReadVariable :: String -> ByteCode ()
+    --WriteVariable :: String -> ByteCode ()
+    {-ReadVariable :: String -> ByteCode ()
     Add :: ByteCode ()
     Sub :: ByteCode ()
     Div :: ByteCode ()
     Multiply :: ByteCode ()
-    ReturnValue :: ByteCode ()
+    ReturnValue :: ByteCode ()-}
+makeEffect ''ByteCode
 
-{- 
-data ByteCode = LoadValue Int
-    | WriteVariable String 
-    | ReadVariable String 
-    | Add 
-    | Sub
-    | Div
-    | Multiply 
-    | ReturnValue
--}
-
-
-{- runByteCode :: Eff '[ByteCode, IO] a -> IO a
-runByteCode = runM . interpretM (\case
-    LoadValue 
-    WriteVariable var -> ...
-    ReadVariale var ->  ...
-    Add -> ...
-    Sub -> ...
-    Div -> ...
-    Multiply -> ...
-    ReturnValue -> ...
-) -}
+runByteCode :: Stack Int -> Eff '[ByteCode] w -> Stack Int
+runByteCode stack req = run $ execState stack (reinterpret go req)
+    where
+        go :: ByteCode v -> Eff '[State (Stack Int)] v
+        go (LoadValue val) = do
+            st <- get
+            let st' = stackPush st val
+            put st'
+            return ()
+        {-go (WriteVariable var) = do
+            st <- get
+            case (stackPop st) of
+                Nothing -> throwError "Error (WriteVariable instruction): the stack is empty"
+                Just (_, val) -> -}
 
 main :: IO ()
-main = someFunc
+main = do
+  let stack = runByteCode (stackNew :: Stack Int) (send $ LoadValue 13)
+  case (stackPop stack) of 
+      Nothing -> putStrLn "Empty stack"
+      Just (_, result) -> putStrLn $ show result
