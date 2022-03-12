d995a14194f3e22c9c2e4c31ead6d31643350670
Omar Mefire <omefire@gmail.com>
2022-03-12 20:14:32 +0100

    Run multiple instructions + Refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -20,23 +20,25 @@ import Control.Monad (when)
 
 
 
-data ByteCode r where
-    LoadValue :: Int -> ByteCode ()
-    WriteVariable :: String -> ByteCode ()
-    ReadVariable :: String -> ByteCode ()
-    Add :: ByteCode ()
-    Sub :: ByteCode ()
-    Div :: ByteCode ()
-    Mul :: ByteCode ()
-    {-ReturnValue :: ByteCode ()-}
-makeEffect ''ByteCode
+data ByteCodeInstruction r where
+    LoadValue :: Int -> ByteCodeInstruction ()
+    WriteVariable :: String -> ByteCodeInstruction ()
+    ReadVariable :: String -> ByteCodeInstruction ()
+    Add :: ByteCodeInstruction ()
+    Sub :: ByteCodeInstruction ()
+    Div :: ByteCodeInstruction ()
+    Mul :: ByteCodeInstruction ()
+    {-ReturnValue :: ByteCodeInstruction ()-}
+makeEffect ''ByteCodeInstruction
 
 type InterpreterState = (Stack Int, Map.Map String Int)
 
-runByteCodeInstruction :: InterpreterState -> Eff '[ByteCode] w -> InterpreterState
-runByteCodeInstruction interpreterState req = run $ execState interpreterState (runError (reinterpret2 go req))
+type ByteCode a = [ByteCodeInstruction a]
+
+runByteCodeInstructionInstruction :: InterpreterState -> Eff '[ByteCodeInstruction] w -> InterpreterState
+runByteCodeInstructionInstruction interpreterState req = run $ execState interpreterState (runError (reinterpret2 go req))
     where
-        go :: ByteCode v -> Eff '[Error (), State InterpreterState] v
+        go :: ByteCodeInstruction v -> Eff '[Error (), State InterpreterState] v
         go (LoadValue val) = do
             (st, map) <- get
             let st' = stackPush st val
@@ -67,18 +69,18 @@ runByteCodeInstruction interpreterState req = run $ execState interpreterState (
             return ()
         go (WriteVariable var) = do
             (st, map) <- get
-            let map' = case (stackPop st :: Maybe(Stack Int, Int)) of
-                            -- TODO: Test this path!
-                            Nothing -> error "Error (WriteVariable instruction): the stack is empty" 
-                            Just (_, val) -> case (Map.lookup var map) of
+            let map' = case (stackPeek st :: Maybe Int) of
+                            Nothing -> error $ "Error (WriteVariable instruction): the stack is empty" <> var
+                            Just val -> case (Map.lookup var map) of
                                                 Nothing -> Map.insert var val map
                                                 Just _ ->  Map.update (\y -> Just y) var map
+                            
             put (st, map')
             return ()
-        go (ReadVariable var) = do
+        go (ReadVariable var) = do -- let result@(resultingStack, resultingMap) = runByteCode [LoadValue 1, WriteVariable "x", ReadVariable "x", LoadValue 1, Add, ReadVariable "y", Mul]
             (st, map) <- get
             let v = case (Map.lookup var map) of
-                        Nothing -> error "Error (ReadVariable instruction): no such variable"
+                        Nothing -> error $ "Error (ReadVariable instruction): no such variable: " <> var
                         Just val -> val
             let st' = stackPush st v
             put (st' :: Stack Int, map :: Map.Map String Int)
@@ -134,32 +136,57 @@ runByteCodeInstruction interpreterState req = run $ execState interpreterState (
             put (stack3 :: Stack Int, map :: Map.Map String Int)
             return ()    
 
-runByteCode :: [ByteCode] -> State InterpreterState ()
-runByteCode [] = 
-runByteCode (x:xs) = 
+
+runByteCode :: ByteCode a -> InterpreterState
+runByteCode [] = (stackNew :: Stack Int, Map.empty)
+runByteCode byteCode@(x:xs) = let initialInterpreterState = (stackNew :: Stack Int, Map.empty)
+    in Prelude.foldl (\interpreterState byteCodeInstruction -> runByteCodeInstructionInstruction interpreterState (send byteCodeInstruction))
+             initialInterpreterState byteCode
 
 main :: IO ()
 main = do
 
+  -- Empty stack and map test
+  putStrLn "---"
+  putStrLn "Empty stack and map test: "
+  putStrLn $ show $ (stackNew :: Stack Int)
+  putStrLn $ show (Map.empty :: Map.Map String Int)
+  putStrLn "---"    
+
   -- LoadValue test
-  let interpreterState = (stackNew :: Stack Int, Map.empty)
-  let (stack, map) = runByteCodeInstruction interpreterState (send $ LoadValue 13)
-  case (stackPop stack) of
-      Nothing -> putStrLn "Empty stack"
-      Just (_, result) -> putStrLn $ "LoadValue test: " ++ show result
+  let (resultingStackLoadValueTest, resultingMapLoadValueTest) = runByteCode [LoadValue 13]
+  putStrLn "LoadValue test: [LoadValue 13]"
+  putStrLn $ show resultingStackLoadValueTest
+  putStrLn $ show resultingMapLoadValueTest
+  putStrLn "---"
 
   -- WriteVariable test
-  let (stack', map') = runByteCodeInstruction (stack, map) (send $ WriteVariable "x")
-  case (Map.lookup "x" map') of
-      Nothing -> putStrLn "WriteVariable test: No variable x"
-      Just val -> putStrLn $ "WriteVariable test: " ++ show val
+  let (resultingStackWriteVariableTest, resultingMapWriteVariableTest) = runByteCode [LoadValue 13, WriteVariable "x"]
+  putStrLn "WriteVariable test: [LoadValue 13, WriteVariable \"x\"]"
+  putStrLn $ show resultingStackWriteVariableTest
+  putStrLn $ show resultingMapWriteVariableTest
+  putStrLn "---"
 
   -- ReadVariable test
-  let (stack'', map'') = runByteCodeInstruction (stack', map') (send $ ReadVariable "x")
-  case (stackPop stack'') of
-      Nothing -> putStrLn "Empty stack"
-      Just (_, result) -> putStrLn $ "ReadVariable test: " ++ show result
+  let (resultingStackReadVariableTest, resultingMapReadVariableTest) = runByteCode [LoadValue 13, WriteVariable "x", ReadVariable "x"]
+  putStrLn "ReadVariable test: [LoadValue 13, WriteVariable \"x\", ReadVariable \"x\"]"
+  putStrLn $ show resultingStackReadVariableTest
+  putStrLn $ show resultingMapReadVariableTest
+  putStrLn "---"
 
   -- Add test
-  -- Sub test
-  -- 
+  let (resultingStackAddTest, resultingMapStackAddTest) = runByteCode [LoadValue 10, LoadValue 3, Mul]
+  putStrLn "Add test: [LoadValue 10, LoadValue 3, Mul]"
+  putStrLn $ show resultingStackAddTest
+  putStrLn $ show resultingMapStackAddTest
+  putStrLn "---"
+
+  -- Sub test (TODO)
+  -- Mul test (TODO)
+
+  -- 'Running multiple instructions' test
+  putStrLn "Multiple instructions test: [LoadValue 1, WriteVariable \"x\", ReadVariable \"x\", LoadValue 1, Add, Mul]"
+  let result@(resultingStack, resultingMap) = runByteCode [LoadValue 1, WriteVariable "x", ReadVariable "x", LoadValue 1, Add, Mul]
+  putStrLn $ show resultingStack
+  putStrLn $ show resultingMap
+  return ()
